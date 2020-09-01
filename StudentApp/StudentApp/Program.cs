c9783a4aace3ce5cc9dabd0c711ab55171027fec
AlekseiKromski <aleksei.kromski@ivkhk.ee>
2020-09-01 11:20:06 +0300

    Make two simple project

---
+++ StudentApp/StudentApp/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace StudentApp
+{
+    class Program
+    {
+        class Student
+        {
+            //Var 1
+            public string firstName;
+            public string lastName;
+            public string group;
+
+            public void GetInfo()
+            {
+                Console.WriteLine($"\nStudent: {firstName} {lastName}, group: {group}");
+            }
+
+        }
+
+        static void Main(string[] args)
+        {
+            //New student object
+            Student s1 = new Student();
+            s1.firstName = "Aleksei";
+            s1.lastName = "Kromski";
+            s1.group = "JPTVR18";
+            s1.GetInfo();
+
+            Console.Write("Press any key ...");
+            Console.ReadKey(); 
+        }
+    }
+}
